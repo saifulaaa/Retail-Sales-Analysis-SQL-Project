0333f9bed804abedd9418f5634b03d9732b79267
saifulaaa <136058848+saifulaaa@users.noreply.github.com>
2025-06-26 12:34:10 +0530

    Update sql_query_p1.sql

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -2,28 +2,9 @@
 CREATE DATABASE sql_project_p2;
 
 
--- Create TABLE
-DROP TABLE IF EXISTS retail_sales;
-CREATE TABLE retail_sales
-            (
-                transaction_id INT PRIMARY KEY,	
-                sale_date DATE,	 
-                sale_time TIME,	
-                customer_id	INT,
-                gender	VARCHAR(15),
-                age	INT,
-                category VARCHAR(15),	
-                quantity	INT,
-                price_per_unit FLOAT,	
-                cogs	FLOAT,
-                total_sale FLOAT
-            );
-
 SELECT * FROM retail_sales
 LIMIT 10
 
-
-    
 
 SELECT 
     COUNT(*) 
@@ -115,16 +96,10 @@ WHERE sale_date = '2022-11-05';
 
 -- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
 
-SELECT 
-  *
+SELECT * 
 FROM retail_sales
-WHERE 
-    category = 'Clothing'
-    AND 
-    TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
-    AND
-    quantity >= 4
-
+WHERE category = "Clothing" AND quantiy >= 4 AND 
+MONTH(sale_date) = 11 AND YEAR(sale_date) = 2022;
 
 -- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
 
@@ -172,10 +147,10 @@ SELECT
 FROM 
 (    
 SELECT 
-    EXTRACT(YEAR FROM sale_date) as year,
-    EXTRACT(MONTH FROM sale_date) as month,
+    YEAR(sale_date) as sales_year,
+    MONTH(sale_date) as sales_month,
     AVG(total_sale) as avg_sale,
-    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
+    RANK() OVER(PARTITION BY YEAR(sale_date) ORDER BY AVG(total_sale) DESC) as rank
 FROM retail_sales
 GROUP BY 1, 2
 ) as t1
@@ -211,8 +186,8 @@ AS
 (
 SELECT *,
     CASE
-        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
-        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN HOUR(sale_time) < 12 THEN 'Morning'
+        WHEN HOUR(sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
         ELSE 'Evening'
     END as shift
 FROM retail_sales
